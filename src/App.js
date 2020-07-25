68e61691c703af61d4cdae163afa40f8a9193dea
Brendon Stephens <brendonstephens93@gmail.com>
2020-07-25 18:33:42 +1000

    inital commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Mang
         </a>
       </header>
     </div>
