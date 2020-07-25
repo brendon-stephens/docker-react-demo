e12b0cb199acf29dc43379b934b47981d5ade0b9
Brendon Stephens <brendonstephens93@gmail.com>
2020-07-25 21:04:16 +1000

    testing feature branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Mang
+          Updated on the feature branch!
         </a>
       </header>
     </div>
